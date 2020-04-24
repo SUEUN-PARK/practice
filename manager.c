9f8db49b683cdb35db90a543bf8b228ff52fd3a6
Park Sueun <21900280@handong.edu>
2020-04-24 17:00:02 +0900

    Implement the loadData()

--- manager.c
+++ manager.c
@@ -56,12 +56,24 @@ void saveData(Product p[], int count){
 //파일에서 데이터 불러오는 함수
 int loadData(Product *p){
 	int count=0;
-	FILE*fp;
+	FILE*fp = fopen("product.txt", "rt");
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
+	if(fp == NULL){
+		printf("=> 파일없음!\n");
+		return 0;
+	}
 
+	while(!feof(fp)){	
 
+		fscanf(fp, "%d %d %[^\n]s", &p[count].weight, &p[count].price, p[count].name);
+		
+		getchar();
 
+		count++;
+	}
+ 
+	fclose(fp);
 
 	printf("=> 로딩 성공!\n");
 	return count;
